b159448c121fbc178531fc2c5b84269331346b4d
retbleed <hsa.lapaz@gmail.com>
2024-03-07 07:41:30 -0700

    Se agregarons los drivers de MongoDB

--- UDapp/obj/Debug/net8.0-ios/iossimulator-arm64/UDapp.AssemblyInfo.cs
+++ UDapp/obj/Debug/net8.0-ios/iossimulator-arm64/UDapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UDapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+66d70a28a230280bc21f6024dfc3ba0b66052477")]
 [assembly: System.Reflection.AssemblyProductAttribute("UDapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UDapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
